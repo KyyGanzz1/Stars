Sorayukii/android_kernel_tama_sdm845

sched: Restore schedtune compatibility
When SCHED_TUNE is disabled, certain elements in userspace, like
libperfmgr, may throw errors when it doesn't detect SCHEDTUNE-related nodes.
So, create a few dummy nodes to satisfy said userspace elements.

Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

include/linux/cgroup_subsys.h
@@ -20,7 +20,7 @@ SUBSYS(cpu)
 SUBSYS(cpuacct)
 #endif
 
-#if IS_ENABLED(CONFIG_CGROUP_SCHEDTUNE)
+#if IS_ENABLED(CONFIG_SCHED_TUNE) || IS_ENABLED(CONFIG_SCHED_TUNE_DUMMY)
 SUBSYS(schedtune)
 #endif
 

KyyGanzz1/Stars

include/linux/cgroup_subsys.h
@@ -20,7 +20,7 @@ SUBSYS(cpu)
 SUBSYS(cpuacct)
 #endif
 
-#if IS_ENABLED(CONFIG_CGROUP_SCHEDTUNE)
+#if IS_ENABLED(CONFIG_CGROUP_SCHEDTUNE) || IS_ENABLED(CONFIG_SCHED_TUNE_DUMMY)
 SUBSYS(schedtune)
 #endif
 